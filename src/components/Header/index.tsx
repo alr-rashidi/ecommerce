cdec448ccf378d247aff9f843f058fd76722c84b
Alireza Rashidi <alr.rashidi@outlook.com>
2024-11-12 19:27:18 +0330

    refactor(header): replace div with nav element

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,12 +6,12 @@ import Buttons from "./buttons";
 
 const Header = () => {
   return (
-    <div className="w-full gap-5 max-w-5xl mx-auto flex justify-between items-center py-4">
+    <nav className="w-full gap-5 max-w-5xl mx-auto flex justify-between items-center py-4">
       <Logo />
       <SearchBar />
       <Navigation />
       <Buttons />
-    </div>
+    </nav>
   );
 };
 
