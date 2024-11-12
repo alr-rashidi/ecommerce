a3cb47d918fcad9b24d78e5473c281be66d022e8
Alireza Rashidi <alr.rashidi@outlook.com>
2024-11-12 17:52:20 +0330

    feat(ui): create button component

--- src/components/Header/buttons.tsx
+++ src/components/Header/buttons.tsx
@@ -1,20 +1,25 @@
-import { FaUserCircle } from "react-icons/fa"
-import { IoCartOutline } from "react-icons/io5"
-import React from 'react'
+import { FaUserCircle } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+import React from "react";
+import Button from "../ui/button";
 
 const Buttons = () => {
   const loggedIn = false;
 
   if (!loggedIn) {
-    return <button className="bg-neutral-800 hover:bg-neutral-600 text-white py-2 px-4 rounded-md transition">Login</button>
+    return (
+      <Button variant="primary" size="sm">
+        Login
+      </Button>
+    );
   }
 
   return (
-    <div className='flex gap-4'>
-      <IoCartOutline className='cursor-pointer size-6' />
-      <FaUserCircle className='cursor-pointer h-full' />
+    <div className="flex gap-4">
+      <IoCartOutline className="cursor-pointer size-6" />
+      <FaUserCircle className="cursor-pointer h-full" />
     </div>
-  )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+type Variants = "primary" | "secondary" | "destructive" | "outline";
+type Sizes = "sm" | "md" | "lg";
+
+const variantMaps: Record<Variants, string> = {
+  primary: "bg-primary text-white",
+  secondary: "bg-secondary text-white",
+  destructive: "bg-destructive text-white",
+  outline:
+    "border border-secondary hover:bg-secondary hover:text-primary text-primary",
+};
+const sizeMaps: Record<Sizes, string> = {
+  sm: "px-4 py-2 text-sm",
+  md: "px-6 py-3 text-base",
+  lg: "px-8 py-4 text-lg",
+};
+
+type ButtonProps = {
+  variant?: Variants;
+  size?: Sizes;
+  children: React.ReactNode;
+  onClick?: () => void;
+  [key: string]: unknown;
+};
+
+const Button = (props: ButtonProps) => {
+  const { variant = "primary", size = "md", children, onClick } = props;
+  return (
+    <button
+      className={`${variantMaps[variant]} ${sizeMaps[size]} rounded-md transition`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
