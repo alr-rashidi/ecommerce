56f77284614085c9f02363266b69e38e766d2fcd
Alireza Rashidi <alr.rashidi@outlook.com>
2024-11-14 22:09:28 +0330

    clean useless codes

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -13,17 +13,6 @@ const inter = Inter({
 const layout = ({ children }: layoutProps) => {
   return (
     <div className={inter.className}>
-      <link
-        rel="stylesheet"
-        type="text/css"
-        charSet="UTF-8"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-      />
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-      />{" "}
       <Header />
       {children}
     </div>
