4685b801525c75a5f70a654622aafd834fa8d873
Alireza Rashidi <alr.rashidi@outlook.com>
2024-12-16 10:47:13 +0330

    feat(customProps): add userProps property

--- src/components/product/productGrid.tsx
+++ src/components/product/productGrid.tsx
@@ -3,16 +3,28 @@ import React from "react";
 import { z } from "zod";
 import ProductCard from "./productCard";
 import Link from "next/link";
+import clsx from "clsx";
 
 type ProductGridProps = {
   products: z.infer<typeof productSchema>[];
   showMoreLink?: string;
-};
-const ProductsGrid = ({ products, showMoreLink }: ProductGridProps) => {
+} & Omit<React.ComponentPropsWithoutRef<"div">, "products" | "showMoreLink">;
+const ProductsGrid = ({
+  products,
+  showMoreLink,
+  className,
+  ...userProps
+}: ProductGridProps) => {
+  const classes = clsx(
+    "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5",
+    className
+  );
+
   if (products.length === 0) return <div>No products found.</div>;
+
   return (
     <div className="flex flex-col gap-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+      <div className={classes} {...userProps}>
         {products.map(product => (
           <ProductCard key={product._id} product={product} />
         ))}
