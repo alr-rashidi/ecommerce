5d7629e8a4515514bf58d247fca73cdc5dda5f30
Alireza Rashidi <alr.rashidi@outlook.com>
2024-12-17 18:32:53 +0330

    feat(hooks): add page parameter to fetchProducts

--- src/hooks/fetchProducts.ts
+++ src/hooks/fetchProducts.ts
@@ -1,20 +1,23 @@
-import { productSchema } from "@/server/validations/product";
+import { APIProductGetType } from "@/app/api/product/route";
 import { Error } from "mongoose";
-import { z } from "zod";
 
 type FetchProductsPropsType = {
   sort?: string;
   limit?: string;
+  page?: number;
 };
 export const fetchProducts = async (
   props?: FetchProductsPropsType
-): Promise<z.infer<typeof productSchema>[] | Error> => {
-  const { sort = "", limit = "8" } = props || {};
+): Promise<APIProductGetType> => {
+  const { sort = "", limit = "12", page = 1 } = props || {};
 
   const url = new URL(`/api/product`, process.env.NEXT_PUBLIC_API_BASE_URL);
   url.searchParams.set("limit", limit);
   if (sort) {
     url.searchParams.set("sort", sort);
+  }
+  if (page) {
+    url.searchParams.set("page", String(page));
   }
 
   try {
