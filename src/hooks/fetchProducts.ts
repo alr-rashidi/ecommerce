ff3f8227f2dad370fec79677e682223c5d162bfe
Alireza Rashidi <alr.rashidi@outlook.com>
2024-12-18 12:06:58 +0330

    feat(fetchProducts): add Category base fetch

--- src/hooks/fetchProducts.ts
+++ src/hooks/fetchProducts.ts
@@ -5,11 +5,12 @@ type FetchProductsPropsType = {
   sort?: string;
   limit?: string;
   page?: number;
+  category?: string;
 };
 export const fetchProducts = async (
   props?: FetchProductsPropsType
 ): Promise<APIProductGetType> => {
-  const { sort = "", limit = "12", page = 1 } = props || {};
+  const { sort = "", limit = "12", page = 1, category } = props || {};
 
   const url = new URL(`/api/product`, process.env.NEXT_PUBLIC_API_BASE_URL);
   url.searchParams.set("limit", limit);
@@ -18,6 +19,9 @@ export const fetchProducts = async (
   }
   if (page) {
     url.searchParams.set("page", String(page));
+  }
+  if (category) {
+    url.searchParams.set("category", category);
   }
 
   try {
