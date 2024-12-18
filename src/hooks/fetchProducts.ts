0c1cc6242efa660728636ade8696b05fb81c2de2
Alireza Rashidi <alr.rashidi@outlook.com>
2024-12-18 16:27:51 +0330

    feat(hooks): add price filtering to fetchProducts hook

--- src/hooks/fetchProducts.ts
+++ src/hooks/fetchProducts.ts
@@ -6,11 +6,20 @@ type FetchProductsPropsType = {
   limit?: number;
   page?: number;
   category?: string;
+  minPrice?: number;
+  maxPrice?: number;
 };
 export const fetchProducts = async (
   props?: FetchProductsPropsType
 ): Promise<APIProductGetType> => {
-  const { sort = "", limit = "12", page = 1, category } = props || {};
+  const {
+    sort = "",
+    limit = "12",
+    page = 1,
+    category,
+    minPrice,
+    maxPrice,
+  } = props || {};
 
   const url = new URL(`/api/product`, process.env.NEXT_PUBLIC_API_BASE_URL);
   url.searchParams.set("limit", String(limit));
@@ -22,6 +31,12 @@ export const fetchProducts = async (
   }
   if (category) {
     url.searchParams.set("category", category);
+  }
+  if (minPrice) {
+    url.searchParams.set("minPrice", String(minPrice));
+  }
+  if (maxPrice) {
+    url.searchParams.set("maxPrice", String(maxPrice));
   }
 
   try {
