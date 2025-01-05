d996c3f63ad0fbeb73431088c4a4e5d7ae00cf58
Alireza Rashidi <alr.rashidi@outlook.com>
2025-01-05 17:26:18 +0330

    refactor(Header): update sub navigation items

--- src/components/Header/subNav.tsx
+++ src/components/Header/subNav.tsx
@@ -5,62 +5,60 @@ import { PiHeadphones } from "react-icons/pi";
 import { IconType } from "react-icons/lib";
 import { TbDeviceMobile } from "react-icons/tb";
 import { BsCamera } from "react-icons/bs";
-import { HiOutlineComputerDesktop } from "react-icons/hi2";
+import { FaLaptop } from "react-icons/fa6";
 
 type ItemsType = {
   title: string;
-  category: string;
+  categoryId: string;
   icon: IconType;
   isActive?: boolean;
 }[];
 const items: ItemsType = [
   {
-    title: "Phones",
-    category: "/phones",
+    title: "SmartPhone",
+    categoryId: "673cb21f936e398308d6f692",
     icon: TbDeviceMobile,
   },
   {
-    title: "Computers",
-    category: "/computers",
-    icon: HiOutlineComputerDesktop,
+    title: "Laptop",
+    categoryId: "673ca8e1936e398308d6f67c",
+    icon: FaLaptop,
   },
   {
-    title: "Smart Watches",
-    category: "/smart-watches",
+    title: "Smartwatch",
+    categoryId: "673ca9b5936e398308d6f684",
     icon: IoWatchOutline,
   },
   {
-    title: "Cameras",
-    category: "/cameras",
+    title: "Camera",
+    categoryId: "673caa3f936e398308d6f68e",
     icon: BsCamera,
   },
   {
-    title: "Headphones",
-    category: "/headphones",
+    title: "Headphone",
+    categoryId: "673ca9a9936e398308d6f682",
     icon: PiHeadphones,
   },
   {
-    title: "Gaming",
-    category: "/gaming",
+    title: "Game Console",
+    categoryId: "673caa5c936e398308d6f690",
     icon: IoGameControllerOutline,
   },
 ];
 
 const SubNav = () => {
   return (
-    <div className="max-lg:hidden bg-primary  py-4 px-14">
+    <div className="max-lg:hidden bg-primary  py-2 px-14">
       <div className="max-w-5xl mx-auto flex items-center justify-between">
         {items.map((item, index) => (
-          <React.Fragment key={item.category}>
-            <div className="flex items-center text-neutral-400 hover:text-neutral-300 gap-2 transition">
+          <React.Fragment key={item.categoryId}>
+            <Link
+              href={`search/${item.categoryId}`}
+              className="flex items-center font-semibold text-xs py-2 px-4 text-neutral-400 hover:text-neutral-300 gap-2 transition"
+            >
               <item.icon />
-              <Link
-                href={`#${item.category}`}
-                className="font-semibold text-xs"
-              >
-                {item.title}
-              </Link>
-            </div>
+              {item.title}
+            </Link>
             {index < items.length - 1 && (
               <div className="w-px h-4 bg-gray-400"></div>
             )}
